f5151a1700ba19d1e8be04ed954af898a4b5f2f1
Andrew <dpieski@gmail.com>
2016-06-04 19:51:15 -0500

    Initial Commit
    This is a basic Powershell script that will update the IP address on Google Dynamic DNS

---
+++ UpdateIP.ps1
@@ -0,0 +1,53 @@
+
+function logger($augs)
+{
+  Out-File -filepath c:\Temp\DynamicDNS.txt -inputobject $augs -encoding ASCII -width 160 -Append
+}
+
+ $ip = (curl icanhazip.com/s).Content
+ $user = ""
+ $pass = ""
+ $hn = ""
+
+$pair = "${user}:${pass}"
+$bytes = [System.Text.Encoding]::ASCII.GetBytes($pair)
+$base64 = [System.Convert]::ToBase64String($bytes)
+$headers = @{ Authorization = "Basic $($base64)"}
+
+$ua = [Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer
+
+
+$body = "POST /nic/update?hostname=$hn&myip=$($ip) HTTP/1.1 "
+$body = $body + "HOST: domains.google.com "
+
+$res = Invoke-WebRequest -Uri "https://$($user):$($pass)@domains.google.com/nic/update?hostname=$($hn)&myip=$($ip)" -Body $body -Method POST -UserAgent $ua -Headers $headers
+
+$result = -split $res
+
+$response = switch ($result[0])
+  {
+    "good" {
+            logger "The update was successful. The IP address $($result[1]) was set."
+            }
+    "nochg" {
+            logger "The supplied IP address was already set."
+            }
+    "nohost" {
+            logger "The hostname does not exist"
+            }
+    "badauth" {
+            logger "The user/pass combo is not valid"
+            }
+    "notfqdn" {
+            logger "The supplied hostname is not a valid fully-qualified domain name"
+            }
+    "badagent" {
+            logger "You Dynamic DNS client is making bad requests."
+            }
+    "abuse" {
+            logger "Dynamic DNS access for the hostname had been blocked due to failure to interpret previous responses."
+            }
+    "911" {
+            logger "An error happened on Google's end. Please wait 5 min. "
+            }
+  }
